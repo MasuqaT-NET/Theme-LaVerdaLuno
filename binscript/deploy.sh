1410d884e413b127a301bafb338fa957fe057841
MasuqaT <occar@hotmail.co.jp>
2016-07-17 23:40:56 +0900

    Git test

--- binscript/deploy.sh
+++ binscript/deploy.sh
@@ -8,7 +8,10 @@ cd ./workspace/library/scss
 compass compile
 cd ../../ # to workspace
 
-COMMIT_MESSAGE=$(git log --format="%s" -n 1 $CIRCLE_SHA1)
+MESSAGE=$(git log --format="%s" -n 1 $CIRCLE_SHA1)
+git stash buf
+git checkout -b dist "${CIRCLE_REPOSITORY_URL}.git"
+git stash pop buf
 git add -A
-git commit -m "Update from CircleCI build No.${CIRCLE_BUILD_NUM} -- ${COMMIT_MESSAGE}"
-git push --force-with-lease --quiet "https://${GH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git" master:dist 2> /dev/null
+git commit -m "Update from CircleCI build No.${CIRCLE_BUILD_NUM} -- ${MESSAGE}"
+git push --force-with-lease --quiet "https://${GH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git" dist 2> /dev/null
