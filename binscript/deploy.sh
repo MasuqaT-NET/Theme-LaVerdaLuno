246985763410618db17550b435038e9a8f30cc44
MasuqaT <occar@hotmail.co.jp>
2016-07-17 23:05:21 +0900

    Update deploy script

--- binscript/deploy.sh
+++ binscript/deploy.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-git clone -b dist --quiet "${CIRCLE_REPOSITORY_URL}.git" workspace
+git clone -b master --quiet "${CIRCLE_REPOSITORY_URL}.git" workspace
 
 cd ./workspace/library/scss
 compass compile
 cd ../../ # to workspace
 
+COMMIT_MESSAGE=$(git log --format="%s" -n 1 $CIRCLE_SHA1)
 git add -A
-git commit -m "Update from CircleCI build no.${CIRCLE_BUILD_NUM}"
+git commit -m "Update from CircleCI build No.${CIRCLE_BUILD_NUM} -- ${COMMIT_MESSAGE}"
 git push --quiet "https://${GH_TOKEN}@github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}.git" dist 2> /dev/null
