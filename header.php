787d5476ab216353f66400ae8ad2a207ecc2fe78
MasuqaT <occar@hotmail.co.jp>
2016-08-16 17:02:20 +0900

    Implement top-bar

--- header.php
+++ header.php
@@ -49,9 +49,28 @@
 					<nav class="site-menu" role="navigation">
 						<div class="menu-toggle"><i class="fa fa-bars"></i></div>
 						<div class="menu-text"></div>
-
-						<?php wp_nav_menu( array( 'container_class' => 'clearfix menu-bar', 'theme_location' => 'primary' ) ); ?>
+						<?php wp_nav_menu(array(
+								'container' => 'div',                             // remove nav container
+								'container_class' => 'menu-bar cf',                 // class of container (should you choose to use it)
+								'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+								'menu_class' => 'nav top-nav cf',               // adding custom nav class
+								'theme_location' => 'main-nav',                 // where it's located in the theme
+								'before' => '',                                 // before the menu
+								'after' => '',                                  // after the menu
+								'link_before' => '',                            // before each link
+								'link_after' => '',                             // after each link
+								'depth' => 0,                                   // limit the depth of the nav
+								'fallback_cb' => ''                             // fallback function (if there is one)
+						)); ?>
 					</nav>
+					<div class="site-search">
+						<div class="search-toggle"><i class="fa fa-search"></i></div>
+						<div class="search-expand">
+							<div class="search-expand-inner">
+								<?php get_search_form(); ?>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 
