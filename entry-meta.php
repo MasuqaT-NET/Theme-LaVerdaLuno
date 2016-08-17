f1c75a2c8aa0ff3769bae634b0b97f7b038fc4b5
MasuqaT <occar@hotmail.co.jp>
2016-08-17 20:41:30 +0900

    Fix date link bug

--- entry-meta.php
+++ entry-meta.php
@@ -15,7 +15,7 @@
 	};
 ?>
 <p class="entry-meta vcard">
-	<span class="posted-on"><i class="fa fa-calendar"></i><a href="<?php echo get_month_link('',''); ?>"><time class="updated entry-time" datetime="<?php echo get_the_time('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_the_time(get_option('date_format')); ?></time></a></span>
+	<span class="posted-on"><i class="fa fa-calendar"></i><a href="<?php echo get_month_link(get_the_time('Y'), get_the_time('m')); ?>"><time class="updated entry-time" datetime="<?php echo get_the_time('Y-m-d'); ?>" itemprop="datePublished"><?php echo get_the_time(get_option('date_format')); ?></time></a></span>
 	<span class="category-link"><i class="fa fa-folder"></i><?php if($categories) { echo implode(', ', array_map($category_formatter, $categories)); } else { echo 'N/A'; } ?></span>
 	<span class="tag-links"><i class="fa fa-tags"></i><?php if($tags) { echo implode(', ', array_map($tag_formatter, $tags)); } else { echo 'N/A'; } ?></span>
 	<span class="comments"><i class="fa fa-comment"></i><?php comments_popup_link('0 Comments', '1 Comment', '% Comments'); ?></span>
