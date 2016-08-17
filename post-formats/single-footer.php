ec3e275076a2f4f6ea8d46cf9f623278bec84cd0
MasuqaT <occar@hotmail.co.jp>
2016-08-17 17:37:56 +0900

    Add ninja omatome button

--- post-formats/single-footer.php
+++ post-formats/single-footer.php
@@ -1,1 +1,14 @@
-<footer class="article-footer"></footer>
+<footer class="article-footer">
+	<div class="ninja_onebutton">
+	<script type="text/javascript">
+	//<![CDATA[
+	(function(d){
+	if(typeof(window.NINJA_CO_JP_ONETAG_BUTTON_fbe46ce8651f06acc4ba37fb41573c3e)=='undefined'){
+		document.write("<sc"+"ript type='text\/javascript' src='\/\/omt.shinobi.jp\/b\/fbe46ce8651f06acc4ba37fb41573c3e'><\/sc"+"ript>");
+	}else{
+		window.NINJA_CO_JP_ONETAG_BUTTON_fbe46ce8651f06acc4ba37fb41573c3e.ONETAGButton_Load();}
+	})(document);
+	//]]>
+	</script><span class="ninja_onebutton_hidden" style="display:none;"><?php the_permalink(); ?></span><span style="display:none;" class="ninja_onebutton_hidden"><?php the_title(); ?></span>
+	</div>
+</footer>
