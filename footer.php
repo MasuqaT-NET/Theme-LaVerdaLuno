59a702182a124faed3235603b80babce563d7c01
MasuqaT <occar@hotmail.co.jp>
2016-08-18 23:32:58 +0900

    Implement footer

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 						)); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					<p class="source-org copyright">&copy; 2012-<?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>, MasuqaT.NET</p>
 
 				</div>
 
