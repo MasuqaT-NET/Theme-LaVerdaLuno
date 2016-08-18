cc80be32aa7c3bdd3326290418c575044469de5c
MasuqaT <occar@hotmail.co.jp>
2016-08-18 13:17:42 +0900

    Add pagenation

--- library/bones.php
+++ library/bones.php
@@ -264,25 +264,24 @@ function bones_related_posts() {
 PAGE NAVI
 *********************/
 
-// Numeric Page Navi (built into the theme by default)
+// Page Navi (built into the theme by default)
 function bones_page_navi() {
   global $wp_query;
   $bignum = 999999999;
-  if ( $wp_query->max_num_pages <= 1 )
+  if ( $wp_query->max_num_pages <= 1 ) {
     return;
-  echo '<nav class="pagination">';
-  echo paginate_links( array(
-    'base'         => str_replace( $bignum, '%#%', esc_url( get_pagenum_link($bignum) ) ),
-    'format'       => '',
-    'current'      => max( 1, get_query_var('paged') ),
-    'total'        => $wp_query->max_num_pages,
-    'prev_text'    => '&larr;',
-    'next_text'    => '&rarr;',
-    'type'         => 'list',
-    'end_size'     => 3,
-    'mid_size'     => 3
-  ) );
-  echo '</nav>';
+  }
+  $current_num = max(1, get_query_var('paged'));
+  ?>
+  <nav id="nav-below" class="cf post-navigation" role="navigation">
+  <?php if ( get_next_posts_link() ) : ?>
+    <div class="nav-previous"><?php next_posts_link('<span class="meta-nav"><i class="fa fa-chevron-left"></i> Previous Articles</span> Page ' . ($current_num + 1)); ?></div>
+  <?php endif;
+  if ( get_previous_posts_link() ) : ?>
+    <div class="nav-next"><?php previous_posts_link('<span class="meta-nav">Next Articles <i class="fa fa-chevron-right"></i></span> Page' . ($current_num - 1)); ?></div>
+  <?php endif; ?>
+  </nav>
+  <?php
 } /* end page navi */
 
 /*********************
